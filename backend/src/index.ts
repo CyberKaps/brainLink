4ee4b53e9b3e60d1b30eb4371706fc401647b2ef
Kalpesh Borde <bordekalpesh@gmail.com>
2025-05-23 00:07:18 +0530

    small mistake solved

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,16 +4,11 @@ import 'dotenv/config'
 import { userRouter } from "./routes/user";
 
 const app = express();
+app.use(express.json());
 
 
+app.use("/api/v1/user", userRouter);
 
-app.use("api/v1/user", userRouter);
 
-
-// app.get("/", (req, res) => {
-//     res.json({
-//         message: "hii there"
-//     })
-// })
 
 app.listen(3001);

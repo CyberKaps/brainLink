dd2d9e75cc057d99fde653ad46e1f50505591f86
Kalpesh Borde <bordekalpesh@gmail.com>
2025-05-22 23:33:50 +0530

    created db schema

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,5 +1,6 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import dotenv from "dotenv"
+import { string, tuple } from "zod";
 
 dotenv.config();
 
@@ -16,3 +17,32 @@ const connectDB = async () => {
 }
 
 connectDB();
+
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true},
+    password: { type: String, required: true},
+
+})
+
+export const User = mongoose.model("User", userSchema); 
+
+
+
+const tagSchema = new mongoose.Schema({
+    title: { type: String }
+})
+
+export const Tag = mongoose.model("Tag", tagSchema);
+
+
+const contentSchema = new mongoose.Schema({
+    title: { type: String, required: true},
+    link: { type: String },
+    tags: { type: mongoose.Types.ObjectId, ref:"Tag"},
+    type: String,
+    userId: { type: mongoose.Types.ObjectId, ref: "User", required: true}
+
+})
+
+export const Content = mongoose.model("Content", contentSchema);
